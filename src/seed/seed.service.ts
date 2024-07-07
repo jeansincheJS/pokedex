fa32d56c3981f99d8823e5b6c91c1df24a59f1a4
JeanS <jean.sinche@alarcongroup.us>
2024-07-07 02:24:51 -0500

    add multiple insert

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,13 +14,16 @@ export class SeedService {
     private readonly pokemonModel: Model<Pokemon>,
   ) { }
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const pokemonToInsert: { name: string, no: number }[] = [];
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no });
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no });
     })
-    return data.results[0];
-    // return 'Seed executed';
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    return 'Seed executed';
   }
 }
