91bf37573b6e12023a4af4fe16551f0df0fd2661
JeanS <jean.sinche@alarcongroup.us>
2024-07-07 02:12:56 -0500

    add axios and interconnect pokemon - seed modules

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) { }
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log({ name, no });
+    })
+    return data.results[0];
+    // return 'Seed executed';
   }
 }
